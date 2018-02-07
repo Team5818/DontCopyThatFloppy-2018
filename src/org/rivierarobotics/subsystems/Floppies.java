a1c7a772bc0265ba0bf7f707823794c4e86e9032
Petey <pdotjpr@gmail.com>
2018-02-06 20:13:06 -0800

    something I guess?

--- src/org/rivierarobotics/subsystems/Floppies.java
+++ src/org/rivierarobotics/subsystems/Floppies.java
@@ -3,13 +3,10 @@ package org.rivierarobotics.subsystems;
 import org.rivierarobotics.commands.RollerControlCommand;
 import org.rivierarobotics.robot.Robot;
 import org.rivierarobotics.robot.RobotMap;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Floppies extends Subsystem {
@@ -60,9 +57,7 @@ public class Floppies extends Subsystem {
 	    int leftSet = (getLeftPos() & 0xFFFFF000) + leftPos;
 	    int rightSet = (getRightPos() & 0xFFFFF000) + rightPos;
 	    left.set(ControlMode.Position, leftSet);
-	    DriverStation.reportError("LR: "+ left.getClosedLoopError(PID_IDX), false);
 	    right.set(ControlMode.Position, rightSet);
-	    DriverStation.reportError("RR: "+right.getClosedLoopError(PID_IDX), false);
 	}
 	
 	public int getLeftPos() {
