b1768d5a207ae00e072f1020c8cf4b788b3424b3
Petey <pdotjpr@gmail.com>
2018-02-10 19:48:58 -0800

    cherrypicking gyro from other branch

--- src/org/rivierarobotics/subsystems/DriveTrain.java
+++ src/org/rivierarobotics/subsystems/DriveTrain.java
@@ -5,6 +5,9 @@ import org.rivierarobotics.constants.RobotMap;
 import org.rivierarobotics.constants.Side;
 import org.rivierarobotics.mathUtil.Vector2d;
 import org.rivierarobotics.robot.Robot;
+import org.rivierarobotics.robot.RobotMap;
+
+import com.ctre.phoenix.sensors.PigeonIMU;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -14,15 +17,17 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class DriveTrain extends Subsystem {
 
-    public DriveTrainSide left;
-    public DriveTrainSide right;
+    private DriveTrainSide left;
+    private DriveTrainSide right;
     public Solenoid shifter;
+    private PigeonIMU gyro;
 
     public enum DriveGear{
         GEAR_LOW, GEAR_HIGH
     }
     
     public DriveTrain() {
+        gyro = new PigeonIMU(RobotMap.GYRO_ID);
         left = new DriveTrainSide(Side.LEFT);
         right = new DriveTrainSide(Side.RIGHT);
         shifter = new Solenoid(RobotMap.SHIFTER_SOLENOID);
@@ -54,7 +59,17 @@ public class DriveTrain extends Subsystem {
     public DriveTrainSide getRightSide() {
         return right;
     }
+    
+    public double getYaw() {
+        double[] ypr = {0,0,0};
+        gyro.getYawPitchRoll(ypr);
+        return ypr[0];
+    }
 
+    public void resetGyro() {
+        gyro.setYaw(0.0, 10);
+    }
+    
     public Vector2d getDistance() {
         return new Vector2d(left.getSidePosition(), right.getSidePosition());
     }
