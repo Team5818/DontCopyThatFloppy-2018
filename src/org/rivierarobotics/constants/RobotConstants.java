fbc75d82c7581715f64db95a60a57868e6645c1e
Petey <pdotjpr@gmail.com>
2018-02-27 21:49:26 -0800

    transfered de-facto wheel diameter

--- src/org/rivierarobotics/constants/RobotConstants.java
+++ src/org/rivierarobotics/constants/RobotConstants.java
@@ -3,7 +3,7 @@ package org.rivierarobotics.constants;
 
 public class RobotConstants {
 
-    public static final double WHEEL_DIAMETER = 6;
+    public static final double WHEEL_DIAMETER = 6.202358;
     public static final double WHEEL_BASE_WIDTH = 26;
     
 }
