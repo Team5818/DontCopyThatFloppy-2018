0f916d98c353533811918c57de5a34c34701d350
Petey <pdotjpr@gmail.com>
2018-02-28 21:09:02 -0800

    cherrypicking from feature/ram-soft-limits

---
+++ src/org/rivierarobotics/commands/SetArmEngaged.java
@@ -0,0 +1,30 @@
+package org.rivierarobotics.commands;
+
+import org.rivierarobotics.robot.Robot;
+import org.rivierarobotics.subsystems.Arm;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class SetArmEngaged extends Command{
+    Arm arm;
+    boolean locked;
+    
+    public SetArmEngaged(boolean l) {
+        arm = Robot.runningRobot.arm;
+        requires(arm);
+        locked = l;
+    }
+    
+    @Override
+    protected void initialize() {
+        arm.setArmEngaged(locked);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return true;
+    }
+    
+    
+    
+}
--- src/org/rivierarobotics/constants/ControlMap.java
+++ src/org/rivierarobotics/constants/ControlMap.java
@@ -1,49 +1,48 @@
 package org.rivierarobotics.constants;
 
-
 public class ControlMap {
-    
+
     public static final int FW_BACK_JOYSTICK_PORT = 0;
-    
+
     public static final int TURN_JOYSTICK_PORT = 1;
-        
+
     public static final int ARM_JOYSTICK_PORT = 2;
-    
+
     public static final int TBD_JOYSTICK_PORT = 3;
-    
+
     public static final int LEFT_BUTTONS_PORT = 4;
 
     public static final int RIGHT_BUTTONS_PORT = 5;
-    
+
     public static final int CLAMP_ON_BUTTON = 1;
-    
+
     public static final int CLAMP_OFF_BUTTON = 2;
 
     public static final int SET_ARM_ANGLE_BUTTON_HIGH = 4;
-    
+
     public static final int SET_ARM_ANGLE_BUTTON_MID = 5;
-    
+
     public static final int SET_ARM_ANGLE_BUTTON_LOW = 6;
 
     public static final int SET_ARM_ANGLE_BUTTON_BACK = 3;
-    
+
     public static final int ZERO_FLAPPYS_BUTTON = 1;
-    
+
     public static final int SHIFT_LOW_BUTTON = 1;
-    
-    public static final int SHIFT_HIGH_BUTTON = 2;
-    
+
+    public static final int SHIFT_HIGH_BUTTON = 2;s
+
     public static final int COLLECT_SEQUENCE_BUTTON = 1;
-    
-    public static final int CLIMB_MODE_BUTTON = 6;
-    
-    public static final int CLIMB_LOCK_BUTTON = 5;
-    
-    public static final int CLIMB_UNLOCK_BUTTON = 4;
-    
+
     public static final int BACK_CAM_BUTTON = 1;
-    
+
     public static final int COLLECT_CAM_BUTTON = 2;
 
-    public static final int FORCE_COMPRESSOR_ON_BUTTON = 2;
+    public static final int CLIMB_MODE_BUTTON = 6;
+
+    public static final int REENGAGE_ARM_BUTTON = 5;
+
+    public static final int DISENGAGE_ARM_BUTTON = 4;
+
+    public static final int LOCK_WINCH_BUTTON = 3;
 }
