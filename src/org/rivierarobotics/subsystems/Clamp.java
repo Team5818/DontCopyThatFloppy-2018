02146b93ae4eb6738e2140f00c9b1bb39cc7ce2b
Jackson McLaughlin <jacksonmclaughlin1@gmail.com>
2018-03-21 19:32:48 -0700

    add puncher to clamp subsys

--- src/org/rivierarobotics/subsystems/Clamp.java
+++ src/org/rivierarobotics/subsystems/Clamp.java
@@ -9,11 +9,13 @@ public class Clamp extends Subsystem {
 
     private Solenoid leftPiston;
     private Solenoid rightPiston;
+    private Solenoid puncher;
     private boolean isOpen;
 
     public Clamp() {
         leftPiston = new Solenoid(RobotMap.LEFT_CLAMP_PISTON_PORT);
         rightPiston = new Solenoid(RobotMap.RIGHT_CLAMP_PISTON_PORT);
+        puncher = new Solenoid(RobotMap.PUNCHER_SOLENOID);
         isOpen = false;
     }
 
@@ -21,6 +23,10 @@ public class Clamp extends Subsystem {
         isOpen = open;
         leftPiston.set(open);
         rightPiston.set(open);
+    }
+    
+    public void setPuncher(boolean extended) {
+    	puncher.set(extended);
     }
     
     public boolean isOpen() {
