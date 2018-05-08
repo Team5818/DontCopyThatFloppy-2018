cda5c9a011b7d3db0f11e080c4026977a6568e36
Jackson McLaughlin <jacksonmclaughlin1@gmail.com>
2018-05-08 15:32:38 -0700

    set 0 power

--- src/org/rivierarobotics/commands/RequireAllSubsystems.java
+++ src/org/rivierarobotics/commands/RequireAllSubsystems.java
@@ -11,19 +11,24 @@ import edu.wpi.first.wpilibj.command.Command;
 public class RequireAllSubsystems extends Command {
 	
 	private final DriveTrain dt = Robot.runningRobot.driveTrain;
-	
 	private final Arm arm = Robot.runningRobot.arm;
 	private final Clamp clamp = Robot.runningRobot.clamp;
 	private final Floppies floppies = Robot.runningRobot.floppies;
 	
-	
 	public RequireAllSubsystems() {
 		requires(dt);
+		requires(arm);
+		requires(clamp);
+		requires(floppies);
 	}
 	
 	@Override
 	protected void execute() {
 		dt.setPowerLeftRight(0, 0);
+		arm.setPower(0);
+		clamp.setOpen(clamp.isOpen());
+		clamp.setPuncher(false);
+		floppies.setPower(0, 0);
 	}
 
 	@Override
